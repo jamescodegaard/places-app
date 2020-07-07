6d4bced8ea72df844f6f895298554c8524b46116
jamescodegaard <jamescodegaard@gmail.com>
2020-07-07 10:33:32 -0500

    adds RESTful routes

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,2 +1,41 @@
 class Api::PlacesController < ApplicationController
+  def index
+    @places = Place.all
+    render 'index.json.jb'
+  end
+
+  def create
+    @place = Place.new(
+      name: params[:name],
+      address: params[:address]
+    )
+    if @place.save
+      render 'show.json.jb'
+    else
+      render json: {message: "Could not save"}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.name = params[:name] || @place.name
+    @place.address = params[:address] || @place.address
+    if @place.save
+      render 'show.json.jb'
+    else
+      render json: {message: "Could not save"}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    render json: {message: "Place was successfully destroyed"}
+  end
+
 end
